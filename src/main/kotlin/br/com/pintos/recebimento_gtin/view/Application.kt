ff61df9722caa4b131405458219cf239c0d9e803
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2019-06-03 06:24:27 -0300

    Atualização

--- src/main/kotlin/br/com/pintos/recebimento_gtin/view/Application.kt
+++ src/main/kotlin/br/com/pintos/recebimento_gtin/view/Application.kt
@@ -1,17 +1,14 @@
 package br.com.pintos.recebimento_gtin.view
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
-@SpringBootApplication
-open class Application: SpringBootServletInitializer() {
-  override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
-    return application.sources(Application::class.java)
-  }
-}
 
-fun main(args: Array<String>) {
-  SpringApplication.run(Application::class.java, *args)
-}
+//@SpringBootApplication(scanBasePackageClasses = [LoginView::class, LoginViewModel::class])
+//open class Application: SpringBootServletInitializer() {
+//  override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+//    return application.sources(Application::class.java)
+//  }
+//}
+
+//fun main(args: Array<String>) {
+//  SpringApplication.run(Application::class.java, *args)
+//}
