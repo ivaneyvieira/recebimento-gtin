171bcd9feae13830ea43c8ccf56da315c60601f2
ivaneyvieira <ivaneyvieira@gmail.com>
2019-08-23 12:33:29 -0300

    Produtos do recebimento

--- src/main/resources/sql/produtosNota.sql
+++ src/main/resources/sql/produtosNota.sql
@@ -1,10 +1,13 @@
 select O.prdno as codigo,  mfno_ref as referencia, TRIM(MID(P.name, 1, 38)) as descricao,
-       SUM(qtty - qttyCancel - qttyRcv) as quant,
+       SUM(O.qtty - qttyCancel - qttyRcv) as quant,
        O.grade, IFNULL(TRIM(B.barcode), '') as gtin,
        MID(grade_l, 1, 10)*1 > 0 OR MID(grade_l, 11, 10)*1 > 0 as temGrade
 from inv AS I
          inner join oprd AS O
                     USING(storeno, ordno)
+         inner join iprd2 AS I2
+                    ON I2.invno = I.l4
+                        AND I2.prdno = O.prdno
          inner join prd AS P
                     on P.no = O.prdno
          left join prdbar AS B
